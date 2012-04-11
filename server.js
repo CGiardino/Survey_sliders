d50e4c579290c1655268335bf1a03a988721ea6b
Carmine Giardino <carmine.giardino@gmail.com>
2012-04-11 18:22:10 +0200

    First commit

---
+++ server.js
@@ -0,0 +1,63 @@
+var express = require('express');
+var io = require('socket.io');
+var csv = require('ya-csv');
+var group=new Array(new Array());
+var reader = csv.createCsvFileReader('csv/extremes.csv', {
+    'separator': ',',
+    'quote': '"',
+    'escape': '"',
+    'comment': '#'
+});
+var z=0;
+reader.addListener('data', function(data) {
+
+
+        group[z]=data;
+
+    z++;
+
+});
+reader.addListener('end',soc);
+
+var app = express.createServer(
+    express.bodyParser()
+    , express.static(__dirname + "/public")
+    , express.cookieParser()
+    , express.session({ secret:'desrever'})
+),io = io.listen(app);
+
+
+app.configure(function () {
+    app.set('views', __dirname + '/views');
+    app.set('view engine', 'jade');
+    app.register('.html', require('jade'));
+    app.use(express.compiler({ src:__dirname + '/views', enable:['less'] }));
+});
+
+
+function soc(){
+
+    io.sockets.on('connection', function (socket) {
+
+
+        for(var i=0;i<group.length;i++)
+            socket.emit('group', group[i]);
+
+
+
+
+    });
+
+
+}
+
+app.get('/', function (req, res) {
+
+
+
+    res.render('index');
+
+
+});
+
+app.listen(3000);
